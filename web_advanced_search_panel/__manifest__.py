7f5db5b2347738ff326c8ffb4a2e03fac7df0fdc
Abdulselam Molla <abdulselam4246@gmail.com>
2026-04-16 20:17:30 +0300

    search panel

--- web_advanced_search_panel/__manifest__.py
+++ web_advanced_search_panel/__manifest__.py
@@ -1,5 +1,5 @@
 {
-    "name": "Web Advanced Search Panel",
+    "name": "Search Panel",
     "version": "17.0.1.0.0",
     "category": "Web",
    "summary": "Display selected search fields inline in the top search bar using context",
