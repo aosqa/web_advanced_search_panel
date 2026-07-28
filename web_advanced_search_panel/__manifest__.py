5dd98327dbc17fb9b1e15c2ea6d783abb298b497
aosqa <abdulselam4246@gmail.com>
2026-07-28 17:43:08 +0300

    Update __manifest__.py

--- web_advanced_search_panel/__manifest__.py
+++ web_advanced_search_panel/__manifest__.py
@@ -1,5 +1,5 @@
 {
-    "name": "Inline Search Panel",
+    "name": "Odoo Quick Search Panel",
     "version": "17.0.1.0.0",
     "category": "Web",
    "summary": "Display selected search fields inline in the top search bar using context",
